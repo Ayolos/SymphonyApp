75ef8dc0c2d6f3806984c6bdbff1e5412b597c78
Antoine <49106208+Ayolos@users.noreply.github.com>
2024-05-12 18:46:49 +0200

    add email verification and change logo

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -6,7 +6,6 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import Logo from "../../../../public/Logo.png";
 
 defineProps({
     status: String,
